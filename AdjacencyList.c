20de126d04f9bda6f5730c19f10eb0e2fccd5d75
Sab <73727767+thesabrinahyap@users.noreply.github.com>
2023-07-19 22:21:28 +0800

    updated add edge pero lacking pa

--- AdjacencyList.c
+++ AdjacencyList.c
@@ -26,10 +26,41 @@ int getHash (String key){
 bool addVertex(AdjacencyList *A, String elem){
 	int ndx= getHash(elem);
 	
-	if(strcmp(A[ndx]->key, EMPTY) == 0){
-		strcpy(A[ndx]->key, elem);
+	while(strcmp(A->List[ndx].key, elem) != 0){
+		if(strcmp(A->List[ndx].key , "EMPTY" != 0 || A->List[ndx].key == "DELETED")){
+			ndx = getHash(++elem) % SIZE;
+		}
+		else{
+			if(strcmp(A->List[ndx].key , "EMPTY" == 0){
+				strcpy(A->List[ndx].key, elem);
+				A->list[ndx].value.status = "OCCUPIED";
+				return true;
+			}
+		}
 	}
+	return false;
 }
-bool addEdge(AdjacencyList *A, String firstElem, String secondElem);
+bool addEdge(AdjacencyList *A, String vertex, String edge){
+	int ctr;
+	bool vertexState = true, edgeState = true;
+	int vNdx = getHash(vertex);
+	int eNdx = getHash(edge);
+	int flagHash;
+	
+	while(strcmp(A->List[vNdx].key, vertex) != 0 && A->List[vNdx].value.status < "EMPTY"){
+		vNdx = (vNdx+1) % SIZE;
+		if(strcmp(A->List[vNdx].key, vertex) == 0){
+			vertexState = true;
+			break;
+		}
+		vertexState = false;
+	}
+	if(strcmp(A->List[vNdx].key, vertex) == 0){
+			vertexState = true;
+		}
+}
+bool deleteVertex(AdjacencyList A, String elem);
+bool deleteVertex(AdjacencyList A, String vertex, String edge);
+void displayList(AdjacencyList A);
 
 
--- AdjacencyList.h
+++ AdjacencyList.h
@@ -6,8 +6,10 @@
 #define ALPHABET_MAX 26
 
 typedef char String[20];
+typedef enum{EMPTY, OCCUPIED, DELETED} hashStatus;
 typedef struct{
 	String* data;
+	hashStatus status;
 	int count;
 	int max;
 }ArrayList;
@@ -17,13 +19,19 @@ typedef struct{
 	ArrayList value;
 }ListType;
 
-typedef ListType AdjacencyList[ALPHABET_MAX];
+typedef struct{
+	ListType List[ALPHABET_MAX];
+}AdjacencyList;
+
+//typedef ListType AdjacencyList[ALPHABET_MAX];
 
 void initList(AdjacencyList *list);
 ArrayList initializeValue(int max);
 int getHash(String key);
 bool addVertex(AdjacencyList A, String elem);
-bool addEdge(AdjacencyList A, String firstElem, String secondElem);
-//to add delete edge and delete vertex
+bool addEdge(AdjacencyList A, String vertex, String edge);
+bool deleteVertex(AdjacencyList A, String elem);
+bool deleteVertex(AdjacencyList A, String vertex, String edge);
+void displayList(AdjacencyList A);
 
 #endif
